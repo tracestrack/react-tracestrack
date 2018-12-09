3e606129c4aa0842c23875fd5a40b3abebaccdef
strongwillow <clear.tsai@gmail.com>
2018-12-09 18:29:29 +0100

    removed placeholder test

--- src/Account.js
+++ src/Account.js
@@ -29,55 +29,55 @@ export class SiteHeader extends React.Component {
 
 	    <header className="masthead">
 
-<nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">Traces</a>
+	      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+		<a className="navbar-brand" href="/">Traces</a>
 
 
-      <div className="collapse navbar-collapse" id="navbarsExample04">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
+		<div className="collapse navbar-collapse" id="navbarsExample04">
+		  <ul className="navbar-nav mr-auto">
+		    <li className="nav-item">
 
-	    {   (this.state.selected=='map') && 
-		    (<a className="nav-link active" href="/map">Map</a>)
+		      {   (this.state.selected=='map') && 
+			  (<a className="nav-link active" href="/map">Map</a>)
 			  ||
-		    (<a className="nav-link" href="/map">Map</a>)
-	    }
+			  (<a className="nav-link" href="/map">Map</a>)
+		      }
 	    
 
-          </li>
-          <li className="nav-item">
-	    {   (this.state.selected=='activities') && 
+            </li>
+		<li className="nav-item">
+		{   (this.state.selected=='activities') && 
 		    (<a className="nav-link active" href="/activities">Activities</a>)
-			  ||
+		    ||
 		    (<a className="nav-link " href="/activities">Activities</a>)
-		  }
+		}
 
-          </li>
-          <li className="nav-item">
+            </li>
+		<li className="nav-item">
+		{
+		    (this.state.selected=='stars') && 
+			(<a className="nav-link active" href="/stars">Stars</a>)
+			||
+			(<a className="nav-link " href="/stars">Stars</a>)
+		}
+
+            </li>
+		<li className="nav-item">
+
 	    {
-		(this.state.selected=='stars') && 
-		  (<a className="nav-link active" href="/stars">Stars</a>)
-		  ||
-		  (<a className="nav-link " href="/stars">Stars</a>)
-	  }
+		    (this.state.selected=='account') && 
+			(<a className="nav-link active" href="/account">Account</a>)
+			||
+			(<a className="nav-link " href="/account">Account</a>)
+		}
 
-          </li>
-          <li className="nav-item">
+            </li>	  
+		</ul>
 
-	    {
-		(this.state.selected=='account') && 
-		  (<a className="nav-link active" href="/account">Account</a>)
-		  ||
-		  (<a className="nav-link " href="/account">Account</a>)
-	  }
+	    </div>
+		</nav>
 
-          </li>	  
-        </ul>
-
-      </div>
-    </nav>
-
-		</header>
+	    </header>
 	);
     }
 }
@@ -86,11 +86,11 @@ export class SiteFooter extends React.Component {
     render() {
 	return (
 
-<footer className="container">
-  <hr className="featurette-divider" />
+	    <footer className="container">
+	      <hr className="featurette-divider" />
 
-        <p>© 2015-2018 Traces · <a href="/privacy">Privacy</a> · <a href="https://blog.traces.website/about/">About</a></p>
-      </footer>
+              <p>© 2015-2018 Traces · <a href="/privacy">Privacy</a> · <a href="https://blog.traces.website/about/">About</a></p>
+	    </footer>
 
 	);
     }
