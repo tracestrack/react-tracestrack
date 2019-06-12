0770d8f220757cdd25d797f4cda4658f57ff20be
strongwillow <clear.tsai@gmail.com>
2019-06-12 23:16:45 +0200

    update package and remove service worker

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './Routing';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Routing />, document.getElementById('root'));
-
-registerServiceWorker();
