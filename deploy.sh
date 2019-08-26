852ab889a2a48bfb8662f983469bd4116af9e722
strongwillow <clear.tsai@gmail.com>
2019-08-26 22:47:30 +0200

    #34 Add test and deliver stages in Jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,13 @@ npm run build
 mv build/index.html build/404.html
 mv build/real_index.html build/index.html
 
-rm ../tracesapp2015.github.io/static/css/*
-rm ../tracesapp2015.github.io/static/js/*
+git clone --depth 1 git@github.com:TracesApp2015/tracesapp2015.github.io.git
 
-cp -r build/* ../tracesapp2015.github.io
-cd ../tracesapp2015.github.io
+rm tracesapp2015.github.io/static/css/*
+rm tracesapp2015.github.io/static/js/*
+
+cp -r build/* tracesapp2015.github.io
+cd tracesapp2015.github.io
 git add .
 git commit -am "update"
 git push
