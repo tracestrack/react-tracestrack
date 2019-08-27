6ca3442743f036a5f5fe8edfa03c41793839a2c1
strongwillow <clear.tsai@gmail.com>
2019-08-27 21:27:31 +0200

    #34 Update deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ npm version patch
 mv build/index.html build/404.html
 mv build/real_index.html build/index.html
 
+rm -rf tracesapp2015.github.io
 git clone --depth 1 git@github.com:TracesApp2015/tracesapp2015.github.io.git
 
 rm tracesapp2015.github.io/static/css/*
