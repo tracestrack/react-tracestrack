e1b28a0abfa34eef78a7f98c84dd79abc427a57b
strongwillow <clear.tsai@gmail.com>
2019-09-21 21:28:59 +0200

    Update deployment repo

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,16 @@
 mv build/index.html build/404.html
 mv build/real_index.html build/index.html
 
-rm -rf tracesapp2015.github.io
-git clone --depth 1 git@github.com:TracesApp2015/tracesapp2015.github.io.git
+rm -rf tracestrack.github.io
+git clone --depth 1 git@github.com:tracestrack/tracestrack.github.io.git
 
-rm tracesapp2015.github.io/static/css/*
-rm tracesapp2015.github.io/static/js/*
+rm tracestrack.github.io/static/css/*
+rm tracestrack.github.io/static/js/*
 
-cp -r build/* tracesapp2015.github.io
-cd tracesapp2015.github.io
+cp -r build/* tracestrack.github.io
+cd tracestrack.github.io
 git add .
 git commit -am "update"
 git push
 
-rm -rf tracesapp2015.github.io
+rm -rf tracestrack.github.io
