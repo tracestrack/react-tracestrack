8d528624f3a47cc369189f3ccb4764b3bc5b5fba
strongwillow <clear.tsai@gmail.com>
2019-09-22 21:22:31 +0200

    #42 Disable serviceWorker

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ git clone --depth 1 git@github.com:tracestrack/tracestrack.github.io.git
 
 rm tracestrack.github.io/static/css/*
 rm tracestrack.github.io/static/js/*
-rm precache-manifest*
 
 cp -r build/* tracestrack.github.io
 cd tracestrack.github.io
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './Routing';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Routing />, document.getElementById('root'));
-serviceWorker.register();
