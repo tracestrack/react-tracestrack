2e4705b14219c38a3d554e82fd1b36cbe545ca06
strongwillow <clear.tsai@gmail.com>
2019-09-22 21:11:43 +0200

    #42 Update service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routing from './Routing';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Routing />, document.getElementById('root'));
+serviceWorker.register();
